b774793b6e9fd5ce655714524e0bb12118476932
tran tri <tranvantri352@gmail.com>
2023-08-23 17:01:23 +0700

    hihi

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        có con cặc em ơi
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sextop1.run"
           target="_blank"
           rel="noopener noreferrer"
         >
